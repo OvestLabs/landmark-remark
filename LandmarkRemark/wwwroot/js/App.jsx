af33c1e7da83e95dbad701163c44fd5cdd5dc3d2
Laith <laith@ovestlabs.com.au>
2017-11-07 15:35:09 +1100

    Added map of melbourne full height.

--- LandmarkRemark/wwwroot/js/App.jsx
+++ LandmarkRemark/wwwroot/js/App.jsx
@@ -3,9 +3,20 @@
 		super(props);
 	}
 
+	componentDidMount() {
+		const melbourne = { lat: -37.811263, lng: 144.963151 };
+		const container = ReactDOM.findDOMNode(this);
+		const options = {
+			center: melbourne,
+			zoom: 15
+		};
+
+		const map = new google.maps.Map(container, options);
+	}
+
 	render() {
 		return (
-			<h1>Landmark React!</h1>
+			<div className="fullHeight"></div>
 		);
 	}
 }
