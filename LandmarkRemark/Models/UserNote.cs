7f2d4e76061dc56bdda8610abd0233b5aec449f4
Laith <laith@ovestlabs.com.au>
2017-11-07 16:53:50 +1100

    Added range validation on Latitude and Longitude

--- LandmarkRemark/Models/UserNote.cs
+++ LandmarkRemark/Models/UserNote.cs
@@ -1,9 +1,15 @@
-﻿namespace LandmarkRemark.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LandmarkRemark.Models
 {
     public sealed class UserNote
     {
+		[Range(-85, 85)]
 		public double Latitude { get; set; }
+
+		[Range(-180, 180)]
 		public double Longitude { get; set; }
+
 		public string Remarks { get; set; }
     }
 }
