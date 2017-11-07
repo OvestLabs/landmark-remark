39d29a419636ccdee242e34b004f9f0415e30f71
Laith <laith@ovestlabs.com.au>
2017-11-07 15:36:27 +1100

    Detecting current location.

--- LandmarkRemark/wwwroot/js/App.jsx
+++ LandmarkRemark/wwwroot/js/App.jsx
@@ -4,14 +4,34 @@
 	}
 
 	componentDidMount() {
-		const melbourne = { lat: -37.811263, lng: 144.963151 };
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const location = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+
+				this.initMap(location);
+			});
+		}
+		else {
+			const melbourne = { lat: -37.811263, lng: 144.963151 };
+			this.initMap(melbourne);
+		}
+	}
+
+	initMap(location) {
 		const container = ReactDOM.findDOMNode(this);
 		const options = {
-			center: melbourne,
+			center: location,
 			zoom: 15
 		};
 
 		const map = new google.maps.Map(container, options);
+		const marker = new google.maps.Marker({
+			position: location,
+			map: map
+		});
 	}
 
 	render() {
