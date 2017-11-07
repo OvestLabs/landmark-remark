9a02b7927dd4c0cacf4da7842e0351e54b6ecec1
Laith <laith@ovestlabs.com.au>
2017-11-07 16:35:15 +1100

    Added notes controller with post support.

---
+++ LandmarkRemark/Controllers/NotesController.cs
@@ -0,0 +1,14 @@
+﻿using LandmarkRemark.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandmarkRemark.Controllers
+{
+	public class NotesController : Controller
+	{
+		[HttpPost]
+		public void Index(UserNote note)
+		{
+
+		}
+	}
+}
--- LandmarkRemark/wwwroot/js/App.jsx
+++ LandmarkRemark/wwwroot/js/App.jsx
@@ -1,42 +1,107 @@
 ﻿class App extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			text: ""
+		};
+
+		const melbourne = { lat: -37.811263, lng: 144.963151 };
+		this.location = melbourne;
+
+		this.handleTextChange = this.handleTextChange.bind(this);
+		this.handleKeyUp = this.handleKeyUp.bind(this);
 	}
 
 	componentDidMount() {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition((position) => {
-				const location = {
+				this.location = {
 					lat: position.coords.latitude,
 					lng: position.coords.longitude
 				};
 
-				this.initMap(location);
+				this.initMap();
 			});
 		}
 		else {
-			const melbourne = { lat: -37.811263, lng: 144.963151 };
-			this.initMap(melbourne);
+			this.initMap();
 		}
 	}
 
-	initMap(location) {
-		const container = ReactDOM.findDOMNode(this);
+	initMap() {
+		const container = ReactDOM.findDOMNode(this).children[0];
 		const options = {
-			center: location,
+			center: this.location,
 			zoom: 15
 		};
 
 		const map = new google.maps.Map(container, options);
 		const marker = new google.maps.Marker({
-			position: location,
+			position: this.location,
 			map: map
 		});
 	}
 
+	submitNote(location, note) {
+		const url = "/notes";
+		const payload = {
+			latitude: location.lat,
+			longitude: location.lng,
+			remark: note
+		};
+
+		//const data = new FormData();
+		//data.append("json", JSON.stringify(payload));
+
+		const options = {
+			method: "POST",
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(payload)
+		};
+
+		console.log(options);
+
+		fetch(url, options)
+			.then(response => { })
+			.catch(error => console.error(error))
+		;
+	}
+
+	handleTextChange(e) {
+		const newText = e.currentTarget.value;
+
+		this.setState({
+			text: newText
+		});
+	}
+
+	handleKeyUp(e) {
+		if (e.key !== "Enter") {
+			return;
+		}
+
+		e.currentTarget.blur();
+
+		this.submitNote(this.location, this.state.text);
+	}
+
 	render() {
 		return (
-			<div className="fullHeight"></div>
+			<div className="fullHeight">
+				<div className="fullHeight"></div>
+				<div className="menu">
+					<input
+						type="text"
+						value={this.state.text}
+						onChange={this.handleTextChange}
+						onKeyUp={this.handleKeyUp}
+						placeholder="enter note here..." />
+				</div>
+			</div>
 		);
 	}
 }
