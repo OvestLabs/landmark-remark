4d695151548e069ea38dafc2b9c0cf41cb350f6f
Laith <laith@ovestlabs.com.au>
2017-11-07 15:45:36 +1100

    Added UserNote model

---
+++ LandmarkRemark/Models/UserNote.cs
@@ -0,0 +1,9 @@
+﻿namespace LandmarkRemark.Models
+{
+    public sealed class UserNote
+    {
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+		public string Remarks { get; set; }
+    }
+}
