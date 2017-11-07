43f4f74ad79abc32225400bf2c56a4059ca4b1a9
Laith <laith@ovestlabs.com.au>
2017-11-07 15:03:32 +1100

    Added index controller.

--- LandmarkRemark/Startup.cs
+++ LandmarkRemark/Startup.cs
@@ -11,6 +11,7 @@ namespace LandmarkRemark
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,10 +22,17 @@ namespace LandmarkRemark
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.Run(async (context) =>
-			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+			app.UseStaticFiles();
+
+			app.UseMvc(
+				routes =>
+				{
+					routes.MapRoute(
+						name: "default",
+						template: "{controller=Home}/{action=Index}/{id?}"
+					);
+				}
+			);
 		}
 	}
 }
