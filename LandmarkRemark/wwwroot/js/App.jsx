8f55fcfcce3b0e3996b309e0a11fbcd9308f8bb4
Laith <laith@ovestlabs.com.au>
2017-11-07 21:57:03 +1100

    Added location watcher.

--- LandmarkRemark/wwwroot/js/App.jsx
+++ LandmarkRemark/wwwroot/js/App.jsx
@@ -11,25 +11,45 @@
 
 		this.handleTextChange = this.handleTextChange.bind(this);
 		this.handleKeyUp = this.handleKeyUp.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleLocationError = this.handleLocationError.bind(this);
 	}
 
 	componentDidMount() {
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				this.location = {
-					lat: position.coords.latitude,
-					lng: position.coords.longitude
-				};
+			const options = {
+				enableHighAccuracy: true,
+				maximumAge: 5000
+			};
 
-				this.initMap();
-			});
+			navigator.geolocation.getCurrentPosition(
+				this.handleLocationChange,
+				this.handleLocationError,
+				options);
+
+			this.watchNumber = navigator.geolocation.watchPosition(
+				this.handleLocationChange,
+				this.handleLocationError,
+				options);
 		}
 		else {
 			this.initMap();
 		}
 	}
 
+	componentWillUnmount() {
+		if (navigator.geolocation) {
+			navigator.geolocation.clearWatch(this.watchNumber);
+		}
+	}
+
 	initMap() {
+		if (this.map) {
+			this.map.panTo(this.location);
+			this.locationMarker.setPosition(this.location);
+			return;
+		}
+
 		const container = ReactDOM.findDOMNode(this).children[0];
 		const options = {
 			center: this.location,
@@ -43,7 +63,7 @@
 			optimized: false,
 			icon: {
 				url: "/assets/location-marker.svg",
-				anchor: new google.maps.Point(50,50)
+				anchor: new google.maps.Point(50, 50)
 			}
 		});
 	}
@@ -81,6 +101,20 @@
 				this.createMarker(position);
 			})
 			.catch(error => console.error(error));
+	}
+
+	handleLocationChange(location) {
+		this.location = {
+			lat: location.coords.latitude,
+			lng: location.coords.longitude
+		};
+
+		this.initMap();
+	}
+
+	handleLocationError(error) {
+		alert(`Error '${error.code}': ${error.message}`);
+		this.initMap();
 	}
 
 	handleTextChange(e) {
